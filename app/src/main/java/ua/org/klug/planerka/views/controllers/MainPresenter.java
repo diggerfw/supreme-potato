2b7e3c34c13c0c129357437ce2ed8a9930c54fac
Andrey Hetsko <andrey.hetsko@gmail.com>
2016-10-21 13:20:49 +0300

    m

--- app/src/main/java/ua/org/klug/planerka/views/controllers/MainPresenter.java
+++ app/src/main/java/ua/org/klug/planerka/views/controllers/MainPresenter.java
@@ -2,7 +2,12 @@ package ua.org.klug.planerka.views.controllers;
 
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 
 import com.android.databinding.library.baseAdapters.BR;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -14,17 +19,22 @@ import com.google.android.gms.location.places.PlacePhotoMetadataResult;
 import com.google.android.gms.location.places.PlacePhotoResult;
 import com.google.android.gms.location.places.Places;
 
+import ua.org.klug.planerka.R;
+
 public class MainPresenter extends BaseObservable {
     private final String KOLOSS_ID = "ChIJ-WsTpTWhJ0ERylq6-OKhL_4";
 
 
     private String mTitle;
+    private Drawable mPlacePhoto;
+    private @DrawableRes int mPlacePhotoPlaceholder = R.mipmap.ic_launcher;
 
     private GoogleApiClient mGoogleApiClient;
 
     public MainPresenter(GoogleApiClient googleApiClient) {
         mGoogleApiClient = googleApiClient;
         getPlaceInfo();
+        receivePlacePhoto();
     }
 
     private void getPlaceInfo() {
@@ -41,21 +51,23 @@ public class MainPresenter extends BaseObservable {
                 );
     }
 
-    private void getPlacePhoto() {
-                Places.GeoDataApi.getPlacePhotos(mGoogleApiClient, KOLOSS_ID).setResultCallback(new ResultCallback<PlacePhotoMetadataResult>() {
+    private void receivePlacePhoto() {
+        Places.GeoDataApi.getPlacePhotos(mGoogleApiClient, KOLOSS_ID).setResultCallback(new ResultCallback<PlacePhotoMetadataResult>() {
+            @Override
+            public void onResult(@NonNull PlacePhotoMetadataResult placePhotoMetadataResult) {
+                PlacePhotoMetadata placePhotoMetadata = placePhotoMetadataResult.getPhotoMetadata().get(0);
+                placePhotoMetadata.getPhoto(mGoogleApiClient).setResultCallback(new ResultCallback<PlacePhotoResult>() {
                     @Override
-                    public void onResult(@NonNull PlacePhotoMetadataResult placePhotoMetadataResult) {
-                        PlacePhotoMetadata placePhotoMetadata = placePhotoMetadataResult.getPhotoMetadata().get(0);
-                        placePhotoMetadata.getPhoto(mGoogleApiClient).setResultCallback(new ResultCallback<PlacePhotoResult>() {
-                            @Override
-                            public void onResult(@NonNull PlacePhotoResult placePhotoResult) {
-                                image.setImageBitmap(placePhotoResult.getBitmap());
-                            }
-                        });
-
-                        placePhotoMetadataResult.getPhotoMetadata().release();;
+                    public void onResult(@NonNull PlacePhotoResult placePhotoResult) {
+                        //image.setImageBitmap(placePhotoResult.getBitmap());
+                        //setPlacePhoto(placePhotoResult.getBitmap());
                     }
                 });
+
+                placePhotoMetadataResult.getPhotoMetadata().release();
+                ;
+            }
+        });
     }
 
     @Bindable
@@ -67,4 +79,15 @@ public class MainPresenter extends BaseObservable {
         mTitle = title;
         notifyPropertyChanged(BR.title);
     }
+
+    @Bindable
+    public Drawable getPlacePhoto() {
+        return mPlacePhoto;
+    }
+
+    public void setPlacePhoto(Bitmap bitmap) {
+        mPlacePhoto = new BitmapDrawable(bitmap);
+        notifyPropertyChanged(BR.placePhoto);
+    }
+
 }
